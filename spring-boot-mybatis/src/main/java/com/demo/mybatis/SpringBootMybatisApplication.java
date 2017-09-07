e06899c155afd27a205979d71d723ad5bb3fac92
ghzhen <gaohaizhen@100bei.com>
2017-09-07 19:31:54 +0800

    no message

--- spring-boot-mybatis/src/main/java/com/demo/mybatis/SpringBootMybatisApplication.java
+++ spring-boot-mybatis/src/main/java/com/demo/mybatis/SpringBootMybatisApplication.java
@@ -1,4 +1,4 @@
-package com.demo.springbootmybatis;
+package com.demo.mybatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
