07a4c0032fd4461a0b30bb79eecd56f250034571
gaohz <gaohaizhen@100bei.com>
2017-12-06 14:46:03 +0800

    init

--- amqp/src/main/java/com/demo/io/StreamDemo.java
+++ amqp/src/main/java/com/demo/io/StreamDemo.java
@@ -6,6 +6,8 @@ public class StreamDemo {
     public static void main(String[] args){
         LocalDate localDate = LocalDate.now();
 
+
+
         System.out.println(localDate.toString());
     }
 
